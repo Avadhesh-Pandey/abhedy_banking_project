70aff68114077ada5bd697bab87d92413cccddc6
bima-avadheshpandey <avadhesh.pandey@milvik.se>
2023-08-25 13:06:03 +0530

    First commit project setup

---
+++ lib/core/constants/color_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColour = Color(0xFF325CA8);
+const kTextGrey = Color(0xFF545454);
+const kTextGreyBg = Color(0xFF787878);
+const kTextBackgroundGrey = Color(0xFFe9e9e9);
